cf0cb818bd985b2377e07d9040d97606a2500985
vyrguskinys <144053476+vyrguskinys@users.noreply.github.com>
2023-11-15 07:01:07 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,46 @@
+main();
+function generateRandomData() {
+
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+    return data;
+    const data = [];
+    });
+    for (let i = 0; i < 10; i++) {
+
+    data.forEach(item => {
+}
+function main() {
+    }
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+}function main() {
+main();
+
+function generateRandomData() {
+}
+    data.forEach(item => {
+}
+
+
+    const data = [];
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    return data;
+    for (let i = 0; i < 10; i++) {
+    });def main():
+import random
+
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+    data = generate_random_data()
+    for item in data:
+    main()
